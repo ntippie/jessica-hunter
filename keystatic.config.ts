bd0e6d00dedd650bfb81cf19d3c29cc1a71a3998
Nate Tippie <nate@tippie.com>
2026-04-26 15:53:13 -0700

    Add astro and keystatic.

---
+++ keystatic.config.ts
@@ -0,0 +1,28 @@
+import { config, fields, collection } from '@keystatic/core';
+
+export default config({
+  storage: {
+    kind: 'github',
+    repo: { owner: 'ntippie', name: 'jessi-hunter' },
+  },
+  collections: {
+    posts: collection({
+      label: 'Posts',
+      slugField: 'title',
+      path: 'src/content/posts/*',
+      format: { contentField: 'content' },
+      schema: {
+        title: fields.slug({ name: { label: 'Title' } }),
+        content: fields.markdoc({
+          label: 'Content',
+          options: {
+            image: {
+              directory: 'src/assets/images/posts',
+              publicPath: '../../assets/images/posts/',
+            },
+          },
+        }),
+      },
+    }),
+  },
+});
